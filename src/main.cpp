42856899e536832ca70369b738ad30fcee8f4639
Kurka Martin <martin.kurka@comap-control.com>
2024-08-06 12:08:40 +0200

    Build for esp32dev
    JIRA: noissue
    
    Signed-off-by: Kurka Martin <martin.kurka.11@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -65,7 +65,7 @@ void setup()
     //Serial1.begin(115200, SERIAL_8N1, rxpin, txpin);
 
     sim800l_init();
-    digitalWrite(15, HIGH);
+    digitalWrite(led_pin, HIGH);
     Serial.println("--------------------------------------------------------");
     Serial.print("reset_reason: ");
     Serial.println(reset_reason);
